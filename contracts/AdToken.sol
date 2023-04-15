54008672fa00b4c2a37116da32471f0be3d255a9
Taro Ishihara <taro.ishihara.mail@gmail.com>
2023-04-15 16:06:12 +0900

    IContents

--- contracts/AdToken.sol
+++ contracts/AdToken.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.9;
 
 import {ByteHasher} from "./helpers/ByteHasher.sol";
 import {IWorldID} from "./interfaces/IWorldID.sol";
+import "./interfaces/IContentsContract.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract AdToken is ERC721, ERC721URIStorage, Ownable {
+contract AdToken is ERC721, ERC721URIStorage, IContentsContract, Ownable {
     using ByteHasher for bytes;
 
     ///////////////////////////////////////////////////////////////////////////////
@@ -105,7 +106,7 @@ contract AdToken is ERC721, ERC721URIStorage, Ownable {
 
     function tokenURI(
         uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    ) public view override(ERC721, ERC721URIStorage, IContentsContract) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 }
